5318a728ca1b91cadbaf0236ce001253fcfd86e0
benedikt-orsolic <benedikt.orsolic@gmail.com>
2020-10-25 19:51:38 +0100

    Start of a login and register system It will later be integrated in a working website

---
+++ login-register.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+
+
+
+<head>
+    <meta name="viewport" content="width=device-widht, initial-scale=1"/>
+    
+    <title> log in</title>
+</head>
+
+
+
+<body>
+
+    <form name="login" method="POST">
+        <input type="text" name="user-name">
+        <input type="password" name="password">
+    </form>
+</body>
+
+
+
+</html>
+
