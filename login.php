1a4b68ea66b4ac1125560fb5beae5bc6590c2027
benedikt-orsolic <benedikt.orsolic@gmail.com>
2020-11-04 17:22:19 +0100

    Changes to be committed: 	modified:   login.css 	modified:   login.php
    	Added some styling and compleated form and css, if I don't change my mind
    	Next is to add client side form validation in real time

--- login.php
+++ login.php
@@ -5,44 +5,77 @@
 
 <head>
     <meta name="viewport" content="width=device-widht, initial-scale=1"/>
-    <link rel="stylesheet" href="login.css"
+    <link rel="stylesheet" href="login.css">
     <title> log in</title>
 </head>
 
 
 
 <body>
-    <form name="login" method="POST">
-        <p>username</p>
+    <h1 id="login-button" onmouseover="switchForm(0)"  style="background-color: #888888">Log in</h1>
+    <h1 id="register-button" onmouseover="switchForm(1)">Register</h1>
+
+    <form id="login-form" name="login" method="POST">
+        <h2>Username:</h2>
         <input type="text" name="user-name">
         <br>
 
-        <p>password</p>
+        <h2>Password:</h2>
         <input type="password" name="password">
         <br>
 
-        <input name="submit" type="button" value="Log in">
+        <input class="submitButton" name="submit" type="button" value="Log in">
     </form>
 
-    <form name="register" method="POST">
-        <p>username</p>
+    <form id="register-form" name="register" method="POST" style="display: none">
+        <h2>Username:</h2>
         <input name="user-name" type="text">
         <br>
 
-        <p>password</p>
+        <h2>Password:</h2>
         <input name="password" type="password">
         <br>
 
-        <p>email</p>
+        <h2>E-mail:</h2>
         <input name="email" type="email">
         <br>
 
-        <input name="submit" type="button" value="Register">
+        <input class="submitButton" name="submit" type="button" value="Register">
 
     </form>
 </body>
 
 
+
+<script>
+    function switchForm(which) {
+
+        var login = document.getElementById('login-form').style;
+        var loginButton = document.getElementById('login-button').style;
+
+        var register = document.getElementById('register-form').style;
+        var registerButton = document.getElementById('register-button').style;
+
+        switch ( which ) {
+            case 0:
+                login.display = 'block';
+                loginButton.backgroundColor = '#888888';
+
+                register.display = 'none';
+                registerButton.backgroundColor = 'white';
+                break;
+            case 1:
+                login.display = 'none';
+                loginButton.backgroundColor = 'white';
+
+                register.display = 'block';
+                registerButton.backgroundColor = '#888888';
+                break;
+        }
+
+        
+    }
+</script>
 
 </html>
 
