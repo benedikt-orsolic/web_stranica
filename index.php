b9ced77d6dfea253826b209468d87a909e067271
benedikt-orsolic <benedikt.orsolic@gmail.com>
2020-11-06 11:54:19 +0100

    Starting nav bar

--- index.php
+++ index.php
@@ -2,12 +2,15 @@
 <html>
 <head>
     <title>Galery</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="index.css">
 </head>
 
 <body>
     <nav>
-        
+        <a href="index.php">Home</a>
+        <a href="my_projects">My projects</a>
+        <a href="login">Log in page</a>
     </nav>
-    <a href="login">Log in page</a>
 <body>
 </html>
