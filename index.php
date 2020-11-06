65fb478a6a203a6059149e3415a7f2a8b54f8785
benedikt-orsolic <benedikt.orsolic@gmail.com>
2020-11-06 14:39:35 +0100

    Fun with nav

--- index.php
+++ index.php
@@ -3,14 +3,42 @@
 <head>
     <title>Galery</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8">
     <link rel="stylesheet" href="index.css">
 </head>
 
 <body>
+    
     <nav>
-        <a href="index.php">Home</a>
-        <a href="my_projects">My projects</a>
-        <a href="login">Log in page</a>
+        <a onclick="togleMenu()" id="navTogle" class="navLink">Nav</a>
+        <a class="navLink"  href="index.php">Home</a>
+        <a class="navLink" href="my_projects">My projects</a>
+        <a class="navLink" href="login">Log in page</a>
     </nav>
 <body>
+
+<script>
+var show = 1;
+
+
+function togleMenu() {
+    
+    var navElements =  document.getElementsByClassName('navLink');
+    var len = navElements.length;
+    var i;
+
+    if ( show ) {
+        for( i=1; i < len; i++ ) {
+            navElements[i].style.display = 'none';
+        }
+        show = 0;
+    } else {
+        for( i=1; i < len; i++ ) {
+            navElements[i].style.display = 'block';
+        }
+        show = 1;
+    }
+}
+</script>
+
 </html>
