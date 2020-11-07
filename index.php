812494117a5896f2225b4296701232c2c33ccbaf
benedikt-orsolic <benedikt.orsolic@gmail.com>
2020-11-07 13:28:04 +0100

    backup

--- index.php
+++ index.php
@@ -12,8 +12,8 @@
     <nav>
         <a onclick="togleMenu()" id="navTogle" class="navLink">Nav</a>
         <a class="navLink"  href="index.php">Home</a>
-        <a class="navLink" href="my_projects">My projects</a>
-        <a class="navLink" href="login">Log in page</a>
+        <a class="navLink" href="my_projects.php">My projects</a>
+        <a class="navLink" href="login.php">Log in page</a>
     </nav>
 <body>
 
