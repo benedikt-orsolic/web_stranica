3d8776958fc88709461509aa427314d84b84fec4
benedikt-orsolic <benedikt.orsolic@gmail.com>
2020-11-20 12:54:20 +0100

    Basic html form for uploading blogPost to server

--- assets/html_lib/blog_post_form.php
+++ assets/html_lib/blog_post_form.php
@@ -1,5 +1,16 @@
-<form >
+<form action="assets/php_lib/upload_blog_post.php" method="post">
+    <label>Blog post title: </label><br>
     <input id='blogPostTitle' type="text" name='title'><br>
+    <label>Blog post body: </label><br>
     <input id='blogPostBody' type='text' name='body'>
+
+
+    <div style='display: inline-block'>
+        <label>Add a nice image to represent your post: </label><br>
+        <input type='file' name='postImage'>
+    </div> 
+    <input type='button' name='submit' value='Post' style="float: right;"></button>
+
+
 </form>
 <br>
