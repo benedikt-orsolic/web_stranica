21fb622df239b14e2f3306fcba4be903cf3805e0
benedikt-orsolic <benedikt.orsolic@gmail.com>
2020-11-20 22:54:58 +0100

    Upload post data to db

--- assets/php_lib/upload_blog_post.php
+++ assets/php_lib/upload_blog_post.php
@@ -1,12 +1,20 @@
 <?php
 
+require_once 'dbConn.inc.php';
+require_once 'blog_post.lib.php';
+
 if( !isset($_POST['submit']) ) {
     header('location: ../../blog.php');
+    die();
 }
 
 $title = $_POST['title'];
 $body = $_POST['body'];
 $postImage = $_POST['postImage'];
+
+//Input sanitise
+
+uploadPost( $title, $body, $postImage, $dbConn );
 
 echo( $title );
 echo( $body );
