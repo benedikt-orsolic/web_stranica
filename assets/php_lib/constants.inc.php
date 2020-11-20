d9fd3d4e94adc176c09a6fbf115873916b0f541a
benedikt-orsolic <benedikt.orsolic@gmail.com>
2020-11-20 22:55:23 +0100

    Update to error loging

--- assets/php_lib/constants.inc.php
+++ assets/php_lib/constants.inc.php
@@ -1,4 +1,4 @@
 <?php
 //file_put_contents ( DEBUG_LOG , 'userName - '.gettype($userName)."\r\n ", FILE_APPEND | LOCK_EX);
-define('DB_ERR_LOG', '../../logs/db.log.txt');
-define('DEBUG_LOG', '../../logs/debug.log.txt');
+define('DB_ERR_LOG', 'logs/db.log.txt');
+define('DEBUG_LOG', 'logs/debug.log.txt');
--- assets/php_lib/dbConn.inc.php
+++ assets/php_lib/dbConn.inc.php
@@ -8,5 +8,5 @@ $dbName = 'personal_web_page';
 $dbConn = mysqli_connect( $serverName, $dbUser, $dbUserPwd, $dbName );
 
 if( !$dbConn ) {
-    file_put_contents ( 'DB_ERR_LOG' , 'dbConn.lib.inc.php - mysqli_connect() fail'."\r\n " , FILE_APPEND | LOCK_EX);
+    file_put_contents ( '../../'.'DB_ERR_LOG' , 'dbConn.lib.inc.php - mysqli_connect() fail'."\r\n " , FILE_APPEND | LOCK_EX);
 }
