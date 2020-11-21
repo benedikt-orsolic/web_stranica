5a0ab7794390c7b83fc4a9ed950e82d331f2eba2
benedikt-orsolic <benedikt.orsolic@gmail.com>
2020-11-21 11:15:48 +0100

    Some updates to bug tracking

--- assets/php_lib/constants.inc.php
+++ assets/php_lib/constants.inc.php
@@ -1,4 +1,13 @@
 <?php
-//file_put_contents ( DEBUG_LOG , 'userName - '.gettype($userName)."\r\n ", FILE_APPEND | LOCK_EX);
+//file_put_contents ( file , msg, FILE_APPEND | LOCK_EX);
+//mysqli_error($dbConn)
+/**
+ * debug.log.txt    // debug info
+ * db_error.log.txt       // DB errors info
+ * error.log.txt    // General errors in code worth loging
+ * 
+ * 
+ * echo date('Y-m-d H:i:s');
+ */
 define('DB_ERR_LOG', 'logs/db.log.txt');
 define('DEBUG_LOG', 'logs/debug.log.txt');
