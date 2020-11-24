9345d87243b7db9f87214f1fcdfced753cafbdff
benedikt-orsolic <benedikt.orsolic@gmail.com>
2020-11-24 16:38:12 +0100

    Move lgoin page css to assets

--- login.php
+++ login.php
@@ -10,64 +10,52 @@
 <!DOCTYPE html>
 <html>
 
-
-
 <head>
     <?php include 'html_lib/global_head_elements.html' ?>
-    <link rel="stylesheet" href="css_lib/login.css">
+    <link rel="stylesheet" href="assets/css_lib/login.css">
 </head>
-
-
 
 <body>
     <?php include 'html_lib/nav.php' ?>
+    <main>
+        <h1 id="login-button" onmouseover="switchForm(0)"  style="background-color: #888888">Log in</h1>
+        <h1 id="register-button" onmouseover="switchForm(1)">Register</h1>
 
-<main>
-    
-    <h1 id="login-button" onmouseover="switchForm(0)"  style="background-color: #888888">Log in</h1>
-    <h1 id="register-button" onmouseover="switchForm(1)">Register</h1>
+        <form id="login-form" action="assets/php_lib/login.inc.php" method="post">
+            <h2>Username:</h2>
+            <input type="text" name="username">
+            <br>
 
-    <form id="login-form" action="assets/php_lib/login.inc.php" method="post">
-        <h2>Username:</h2>
-        <input type="text" name="username">
-        <br>
+            <h2>Password:</h2>
+            <input type="password" name="password">
+            <br>
 
-        <h2>Password:</h2>
-        <input type="password" name="password">
-        <br>
+            <input type="hidden" name="login" value="0"/>
+            <input class="submitButton" name="submit" type="submit" value="Log in">
+        </form>
 
-        <input type="hidden" name="login" value="0"/>
-        <input class="submitButton" name="submit" type="submit" value="Log in">
-    </form>
+        <form id="register-form"  action="assets/php_lib/register.inc.php" method="post" style="display: none;">
+            <h2>Username:</h2>
+            <input id="registerName" name="userName" type="text" onkeyup="validateUsername()">
+            <p id="invalidUserName" class="invalidInput"></p>
+            <br>
 
-    <form id="register-form"  action="assets/php_lib/register.inc.php" method="post" style="display: none;">
-        <h2>Username:</h2>
-        <input id="registerName" name="userName" type="text" onkeyup="validateUsername()">
-        <p id="invalidUserName" class="invalidInput"></p>
-        <br>
+            <h2>Password:</h2>
+            <input id="registerPassword" name="password" type="text" onkeyup="validatePassword()">
+            <p id="invalidPassword" class="invalidInput"></p>
+            <br>
 
-        <h2>Password:</h2>
-        <input id="registerPassword" name="password" type="text" onkeyup="validatePassword()">
-        <p id="invalidPassword" class="invalidInput"></p>
-        <br>
+            <h2>E-mail:</h2>
+            <input id="registerEmail" name="email" type="email" oninput="validateEmail()">
+            <br>
 
-        <h2>E-mail:</h2>
-        <input id="registerEmail" name="email" type="email" oninput="validateEmail()">
-        <br>
-
-        <input type="hidden" name="register" value="0"/>
-        <input class="submitButton" name="submit" type="submit" value="Register">
-
-    </form>
-
-    <?php if( isset($_GET['err'])) echo('<p class="invalidInput">' . $_GET['err'] . '</p>'); ?>
-
-
-    
-</main>
+            <input type="hidden" name="register" value="0"/>
+            <input class="submitButton" name="submit" type="submit" value="Register">
+        </form>
+        <?php if( isset($_GET['err'])) echo('<p class="invalidInput">' . $_GET['err'] . '</p>'); ?>
+        
+    </main>
 </body>
-
-
 
 <script src="assets/js_lib/login-register.js"></script>
 
