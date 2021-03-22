8606fccc7cadf79ccf03079a3d04992eef73abc4
benedikt-orsolic <benedikt.orsolic@gmail.com>
2021-03-22 02:27:34 +0100

    A bit of base article structure output clean up Will make it easier to debug raw server respons

--- assets/php_lib/get_blog_post.php
+++ assets/php_lib/get_blog_post.php
@@ -49,10 +49,9 @@ while($row = mysqli_fetch_assoc($result)){
     $markDownToHtml->setStr( $row['text'] );
     file_put_contents ( "../../logs/debug.log" , "h\n", FILE_APPEND | LOCK_EX);
 
-    echo( '<article id="blogPost=' .$row['upid']. '" class="blogPost">');
-    echo( '<h2 class="postTiele">'.$row['title'].'</h2>' );
-    echo( '<section class="postText">'. $markDownToHtml->getStr() .'</section>' );
-    echo( '</article>'); 
+    echo( '<article id="blogPost=' .$row['upid']. '" class="blogPost">' . "\n");
+    echo( '<h2 class="postTiele">'.$row['title'].'</h2>' . "\n" );
+    echo( '<section class="postText">' . "\n\n" . $markDownToHtml->getStr() . '</section></article>' . "\n\n" );
 }
 
 mysqli_stmt_close( $stmt );
