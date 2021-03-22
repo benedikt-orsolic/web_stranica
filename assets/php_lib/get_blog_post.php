ef2ee61debe3952baeda234fee9bd55cbbe4369e
benedikt-orsolic <benedikt.orsolic@gmail.com>
2021-03-22 02:12:39 +0100

    Markdown converter oop, headings work

--- assets/php_lib/get_blog_post.php
+++ assets/php_lib/get_blog_post.php
@@ -42,11 +42,16 @@ if( $upid != -1 ) {
     $result = mysqli_stmt_get_result( $stmt );
 }
 
+$markDownToHtml = new MarkDownToHtml();
+
 while($row = mysqli_fetch_assoc($result)){
     
+    $markDownToHtml->setStr( $row['text'] );
+    file_put_contents ( "../../logs/debug.log" , "h\n", FILE_APPEND | LOCK_EX);
+
     echo( '<article id="blogPost=' .$row['upid']. '" class="blogPost">');
     echo( '<h2 class="postTiele">'.$row['title'].'</h2>' );
-    echo( '<section class="postText">'.getWithMarkDownToHTML( $row['text'] ).'</section>' );
+    echo( '<section class="postText">'. $markDownToHtml->getStr() .'</section>' );
     echo( '</article>'); 
 }
 
@@ -66,135 +71,145 @@ mysqli_stmt_close( $stmt );
     new lines separate paragraphs
     [color:#ffffff]{text} or [color:blue]
 */
-function getWithMarkDownToHTML( $str ) {
-
-    $len = strlen($str);
-    $result = "";
-    $lastPos = 0;
+class MarkDownToHtml{    
     
-    //Lowest heading
-    do{
-        $i = strpos($str, "###");
-        $j = strpos($str, "\n", $i + 1);
+    private string $str;
+
+    public function setStr( string $str ) {
         
-        // new line char at index $j has to be left or paragraph wont be recognised later
-        if( $i !== false && $j !== false ) $str = substr($str, 0, $i) . "<h5>" . substr($str, $i+3, $j - $i - 1) . "</h5>" . substr($str, $j-1, $len);
-        else break;
-    } while(1);
-
-    //Middle heading
-    do{
-        $i = strpos($str, "##");
-        $j = strpos($str, "\n", $i + 1);
-        // new line char at index $j has to be left or paragraph wont be recognised later
-        if( $i !== false && $j !== false ) $str = substr($str, 0, $i) . "<h4>" . substr($str, $i+2, $j - $i - 1) . "</h4>" . substr($str, $j-1, $len);
-        else break;
-    } while(1);
-
-    //Highest heading
-    do{
-        $i = strpos($str, "#");
-        $j = strpos($str, "\n", $i + 1);
-        // new line char at index $j has to be left or paragraph wont be recognised later
-        if( $i !== false && $j !== false ) $str = substr($str, 0, $i) . "<h3>" . substr($str, $i+1, $j - $i - 1) . "</h3>" . substr($str, $j-1, $len);
-        else break;
-    } while(1);
-
-    //Color
-    do{
-        $i = strpos($str, "[color:");
-        $j = strpos($str, "]", $i + 1);
-
-        $color = "";
-        if( $i !== false && $j !== false ) $color = substr($str, $i+7, $j - $i - 7);
-        else break;
         
-        $textOpen = strpos($str, "{");
-        $textClose = strpos($str, "}");
+        $this->str = $str;
+    }
 
-        if( $textOpen !== false && $textClose !== false && $i !== false && $j !== false ) {
-            $str = 
-            substr($str, 0, $i) . 
-            "<span style='color: " . 
-            $color . "'>" . 
-            substr($str, $textOpen+1, $textClose - $textOpen - 1) . 
-            "</span>" . 
-            substr($str, $textClose+1, $len);
+    public function getStr() {
+        $this->getWithMarkDownToHTML();
+        return $this->str;
+    }
+    
+    private function getWithMarkDownToHTML() {
+        
+        //Lowest heading
+        do{
+            
+            // new line char at index $j has to be left or paragraph wont be recognised later
+            if( $this->markDownSubstitute(0, 3, -1, -1, "###", "\n", 1, "<h5>", "</h5>") ) continue;
+            else break;
+        } while(1);
+
+        //Middle heading
+        do{
+
+            // new line char at index $j has to be left or paragraph wont be recognised later
+            if( $this->markDownSubstitute(0, 2, -1, -1, "##", "\n", 1, "<h4>", "</h4>") ) continue;
+            else break;
+        } while(1);
+
+        //Highest heading
+        do{
+
+            // new line char at index $j has to be left or paragraph wont be recognised later
+            if( $this->markDownSubstitute(0, 1, -1, -1, "#", "\n", 1, "<h3>", "</h3>") ) continue;
+            else break;
+        } while(1);
+        /*
+        //Color
+        do{
+            $i = strpos($str, "[color:");
+            $j = strpos($str, "]", $i + 1);
+
+            $color = "";
+            if( $i !== false && $j !== false ) $color = substr($str, $i+7, $j - $i - 7);
+            else break;
+            
+            $textOpen = strpos($str, "{");
+            $textClose = strpos($str, "}");
+
+            if( $textOpen !== false && $textClose !== false && $i !== false && $j !== false ) {
+                $str = 
+                substr($str, 0, $i) . 
+                "<span style='color: " . 
+                $color . "'>" . 
+                substr($str, $textOpen+1, $textClose - $textOpen - 1) . 
+                "</span>" . 
+                substr($str, $textClose+1, $len);
+            }
+            else break;
+        } while(1);
+
+        //Bold and italic text
+        do{
+
+            // new line char at index $j has to be left or paragraph wont be recognised later
+            if( $this->markDownSubstitute(0, 3, -3, -3, "***", "***", 1, "<strong><em>", "</em></strong>") ) continue;
+            else break;
+        } while(1);
+
+        //Bold text
+        do{
+
+            if( $this->markDownSubstitute(0, 2, -2, -2, "**", "**", 1, "<strong>", "</strong>") ) continue;
+            else break;
+        } while(1);
+
+        //Italic
+        do{
+
+            if( $this->markDownSubstitute(0, 1, -1, +1, "*", "*", 1, "<em>", "</em>") ) continue;
+            else break;
+        } while(1);
+
+        //Horizontal rule
+        // Add 2 new lines in front and back to help paragraph section detect it since firefox doesn't like it as <p> <hr> </p>
+        do{
+
+            if( $this->markDownSubstitute(0, 0, 0, +3, "---", "---", 0, "\r\n\r\n<hr>\r\n\r\n", "") ) continue;
+            else break;
+        } while(1);
+
+        //Line break
+        // Add 2 new lines in front and back to help paragraph section detect it since firefox doesn't like it as <p> <hr> </p>
+        do{
+            if( $this->markDownSubstitute(0, 0, 0, +3, "  ", "  ", 0, "<br>", "") ) continue;
+            else break;
+        } while(1);
+
+
+        //Paragraphs
+        do{
+            $i = strpos($this->str, "\r\n\r\n");
+            $j = strpos($this->str, "\r\n\r\n", $i + 4);
+
+            // Escape <hr> as it can't be inside <p>
+            if( strpos($this->str, "<hr>", $i) < $j) {
+                $this->str = substr($this->str, 0, $i) . substr($this->str, $i+4, $len);
+                continue;
+            }
+            
+            // Leave trailing "\r\n\r\n" so it can be picked up for next paragraph, potentionally picks up a heading in it, best i got for now
+            if( $i !== false && $j !== false ) $this->str = substr($this->str, 0, $i) . "<p>" . substr($this->str, $i+4, $j - $i - 4) . "</p>\n" . substr($this->str, $j, $len);
+            else break;
+        } while(1);
+        */
+    }
+
+
+    private function markDownSubstitute(int $start_endOffset, int $mid_stratOffest, int $mid_endOffset, int $end_startOffset, 
+                                string $openMark, string $closeMark, int $closeMark_offset, 
+                                string $openHTML, string $closeHTML) {
+        
+        $i = strpos($this->str, $openMark);
+        $j = strpos($this->str, $closeMark, $i + $closeMark_offset);
+
+        if( $i !== false && $j !== false ){
+            $this->str = substr($this->str, 0, $i + $start_endOffset) . 
+                         $openHTML . 
+                         substr($this->str, $i + $mid_stratOffest, $j - $i + $mid_endOffset) . 
+                         $closeHTML . 
+                         substr($this->str, $j + $end_startOffset, strlen($this->str) );
+            
+            return 1;
+        } else {
+            return 0;
         }
-        else break;
-    } while(1);
-
-     //Bold and italic text
-     do{
-        $i = strpos($str, "***");
-        $j = strpos($str, "***", $i + 1);
-
-        if( $i !== false && $j !== false ) $str = substr($str, 0, $i) . "<strong><em>" . substr($str, $i+3, $j - $i - 3) . "</em></strong>" . substr($str, $j+3, $len);
-        else break;
-    } while(1);
-
-    //Bold text
-    do{
-        $i = strpos($str, "**");
-        $j = strpos($str, "**", $i + 1);
-
-        if( $i !== false && $j !== false ) $str = substr($str, 0, $i) . "<strong>" . substr($str, $i+2, $j - $i - 2) . "</strong>" . substr($str, $j+2, $len);
-        else break;
-    } while(1);
-
-    //Italic
-    do{
-        $i = strpos($str, "*");
-        $j = strpos($str, "*", $i + 1);
-
-        if( $i !== false && $j !== false ) $str = substr($str, 0, $i) . "<em>" . substr($str, $i+1, $j - $i - 1) . "</em>" . substr($str, $j+1, $len);
-        else break;
-    } while(1);
-
-    //Horizontal rule
-    // Add 2 new lines in front and back to help paragraph section detect it since firefox doesn't like it as <p> <hr> </p>
-    do{
-        $i = strpos($str, "---");
-
-        if( $i !== false ) $str = substr($str, 0, $i) . "\r\n\r\n<hr>\r\n\r\n" . substr($str, $i+3, $len);
-        else break;
-    } while(1);
-
-    //Line break
-    // Add 2 new lines in front and back to help paragraph section detect it since firefox doesn't like it as <p> <hr> </p>
-    do{
-        $i = strpos($str, "  ");
-
-        if( $i !== false ) $str = substr($str, 0, $i) . "<br>" . substr($str, $i+3, $len);
-        else break;
-    } while(1);
-
-
-    //Paragraphs
-    do{
-        $i = strpos($str, "\r\n\r\n");
-        $j = strpos($str, "\r\n\r\n", $i + 4);
-
-        // Escape <hr> as it can't be inside <p>
-        if( strpos($str, "<hr>", $i) < $j) {
-            $str = substr($str, 0, $i) . substr($str, $i+4, $len);
-            continue;
-        }
-        
-        // Leave trailing "\r\n\r\n" so it can be picked up for next paragraph, potentionally picks up a heading in it, best i got for now
-        if( $i !== false && $j !== false ) $str = substr($str, 0, $i) . "<p>" . substr($str, $i+4, $j - $i - 4) . "</p>\n" . substr($str, $j, $len);
-        else break;
-    } while(1);
-
-    return $str;
-}
-
-
-function MarkDownSubstitute(string $str, int $start_endOffset, int $mid_stratOffest, int $mid_endOffset, int $end_startOffset, string $openTag, string $closeTag) {
-    return 
-        substr($str, 0, $i + $start_endOffset) . 
-        $openTag . 
-        substr($str, $i + $mid_stratOffest, $j - $i + $mid_endOffset) . 
-        $closeTag . 
-        substr($str, $j + $end_startOffset, strlen($str) );
+    }
 }
