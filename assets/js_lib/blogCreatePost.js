c4784ebd10dd371adb4e73df5b271ffafac2ee96
benedikt-orsolic <benedikt.orsolic@gmail.com>
2021-03-29 20:49:50 +0200

    Show post edditor after post is created in db

--- assets/js_lib/blogCreatePost.js
+++ assets/js_lib/blogCreatePost.js
@@ -9,10 +9,10 @@ if (document.addEventListener) {                // For all major browsers, excep
 function createNewPost() {
     const xhttp = new XMLHttpRequest();
 
-    xhttp.onreadystatechange = ()=>{
-
-        if(this.status == 200) postCreateSuccess();
-        if(this.status == 400) postCreateFail();
+    xhttp.onreadystatechange = function(){
+        
+        if(this.readyState == 4 && this.status == 200) postCreateSuccess();
+        if(this.readyState == 4 && this.status == 400) postCreateFail();
     };
     const data = new FormData();
 
@@ -26,7 +26,7 @@ function createNewPost() {
 
 
 function postCreateSuccess() {
-
+    document.getElementById('blogEditor').style.display = 'block';
 }
 
 
