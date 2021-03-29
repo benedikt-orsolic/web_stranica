9264db78a0054f8859da70eb3a732d4351d5fb7d
benedikt-orsolic <benedikt.orsolic@gmail.com>
2021-03-29 02:36:20 +0200

    Store file on the server

--- assets/php_lib/upload_blog_post_image.php
+++ assets/php_lib/upload_blog_post_image.php
@@ -1,4 +1,18 @@
 <?php
-if( isset( $_FILES['imgUpload']) ) {
-    echo ( $_FILES['imgUpload']['type'][0]);
+
+include 'auto_loader.inc.php';
+
+var_dump( $_FILES['imgUpload']['name']) ;
+if( !isset( $_FILES['imgUpload']) && $_FILES['imgUpload']['error'] === 0) {
+    http_response_code(400);
+    echo('Bad request');
+    die();
+}
+
+
+$blogImageHandler = new ImageHandler();
+if(!$blogImageHandler->processImgFile($_FILES['imgUpload'])) {
+    http_response_code(400);
+    echo('Bad request');
+    die();
 }
