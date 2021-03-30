cee886d1decac0f5b005bca8ca23f2540c074a7b
benedikt-orsolic <benedikt.orsolic@gmail.com>
2021-03-30 20:28:45 +0200

    File rename

---
+++ assets/php_lib/blogUploadPostImage.php
@@ -0,0 +1,38 @@
+<?php
+
+include 'auto_loader.inc.php';
+session_start();
+
+
+
+if( !isset($_SESSION['uuid'])) {
+    http_response_code(401);
+    die();
+}
+
+if( (!isset( $_FILES['imgUpload']) && $_FILES['imgUpload']['error'] === 0)  || 
+     !isset($_POST['upid'])) {
+    http_response_code(400);
+    die();
+}
+
+
+
+$quarryBlogPost = new QuarryBlogPost();
+$postRaw = $quarryBlogPost->getLastNPosts($_POST['upid'], 1)[0];
+$postOwner = $postRaw['ownerId'];
+$postId = $postRaw['upid'];
+
+if( $postOwner != $_SESSION['uuid'] ) {
+    http_response_code(401);
+    die();
+}
+
+
+
+$blogImageHandler = new ImageHandler();
+if(!$blogImageHandler->processImgFile($postId, $_FILES['imgUpload'])) {
+    http_response_code(400);
+    echo('Bad request');
+    die();
+}
