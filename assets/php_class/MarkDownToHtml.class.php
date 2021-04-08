630bd0540e4d5f1720685bb5bafa56552690f512
benedikt-orsolic <benedikt.orsolic@gmail.com>
2021-04-08 14:14:53 +0200

    Move img and link syntax to function

--- assets/php_class/MarkDownToHtml.class.php
+++ assets/php_class/MarkDownToHtml.class.php
@@ -40,76 +40,38 @@ class MarkDownToHtml{
         $this->findAndSubstitute(0, 1, -1, 0, "#", "\n", 1, "<h3>", "</h3>");
         
         //Color
-        do{
-            $i = strpos($this->str, "[color:");
-            $j = strpos($this->str, "]", $i + 1);
+        // do{
+        //     $i = strpos($this->str, "[color:");
+        //     $j = strpos($this->str, "]", $i + 1);
 
-            $color = "";
-            if( $i !== false && $j !== false ) $color = substr($this->str, $i+7, $j - $i - 7);
-            else break;
+        //     $color = "";
+        //     if( $i !== false && $j !== false ) $color = substr($this->str, $i+7, $j - $i - 7);
+        //     else break;
             
-            $textOpen = strpos($this->str, "{");
-            $textClose = strpos($this->str, "}");
+        //     $textOpen = strpos($this->str, "{");
+        //     $textClose = strpos($this->str, "}");
 
-            if( $textOpen !== false && $textClose !== false && $i !== false && $j !== false ) {
-                $this->str = 
-                substr($this->str, 0, $i) . 
-                "<span style='color: " . 
-                $color . "'>" . 
-                substr($this->str, $textOpen+1, $textClose - $textOpen - 1) . 
-                "</span>" . 
-                substr($this->str, $textClose+1, strlen($this->str));
-            }
-            else break;
-        } while(1);
+        //     if( $textOpen !== false && $textClose !== false && $i !== false && $j !== false ) {
+        //         $this->str = 
+        //         substr($this->str, 0, $i) . 
+        //         "<span style='color: " . 
+        //         $color . "'>" . 
+        //         substr($this->str, $textOpen+1, $textClose - $textOpen - 1) . 
+        //         "</span>" . 
+        //         substr($this->str, $textClose+1, strlen($this->str));
+        //     }
+        //     else break;
+        // } while(1);
 
         
 
         //Img
-        do{
-            $i = strpos($this->str, "![");
-            $j = strpos($this->str, "]", $i + 1);
-
-            if( $i !== false && $j !== false ) $imgAlt = substr($this->str, $i + 2, $j - $i - 2);
-            else break;
-            
-            $urlOpen = strpos($this->str, "(");
-            $urlClose = strpos($this->str, ")");
-
-            if( $urlOpen !== false && $urlClose !== false && $i !== false && $j !== false ) $url = substr($this->str, $urlOpen + 1, $urlClose - $urlOpen - 1) ;
-            else break;
-            
-            $this->str = 
-                substr($this->str, 0, $i) . 
-                "<img class=\"blogPostImage\" src=\"" . $url . "\" alt=\"" . $imgAlt . "\">" .
-                substr($this->str, $urlClose+1, strlen($this->str));
-            
-        } while(1);
+        $this->findAndSubstituteBlock("!");
         
 
 
         //Link
-        do{
-            $i = strpos($this->str, "[");
-            $j = strpos($this->str, "]", $i + 1);
-
-            $linkText = "";
-
-            if( $i !== false && $j !== false ) $linkText = substr($this->str, $i + 1, $j - $i - 1);
-            else break;
-            
-            $urlOpen = strpos($this->str, "(");
-            $urlClose = strpos($this->str, ")");
-
-            if( $urlOpen !== false && $urlClose !== false && $i !== false && $j !== false ) $url = substr($this->str, $urlOpen + 1, $urlClose - $urlOpen - 1) ;
-            else break;
-            
-            $this->str = 
-                substr($this->str, 0, $i) . 
-                "<a href=\"" . $url . "\">" . $linkText . "</a>" .
-                substr($this->str, $urlClose+1, strlen($this->str));
-            
-        } while(1);
+        $this->findAndSubstituteBlock("");
 
 
         //Bold and italic text
@@ -131,22 +93,66 @@ class MarkDownToHtml{
         $this->findAndSubstitute(0, 0, 0, +2, "  ", "  ", 0, "<br>\n", "");
 
 
-        //Paragraphs
-        do{
-            $i = strpos($this->str, "\r\n\r\n");
-            $j = strpos($this->str, "\r\n\r\n", $i + 4);
+        //TODO: Paragraphs not working
+        // do{
+        //     $i = strpos($this->str, "\r\n\r\n");
+        //     $j = strpos($this->str, "\r\n\r\n", $i + 4);
 
-            // Escape <hr> as it can't be inside <p>
-            if( strpos($this->str, "<hr>", $i) < $j) {
-                $this->str = substr($this->str, 0, $i) . substr($this->str, $i+4, strlen($this->str));
-                continue;
-            }
+        //     // Escape <hr> as it can't be inside <p>
+        //     // if( strpos($this->str, "<hr>", $i) < $j) {
+        //     //     $this->str = substr($this->str, 0, $i) . substr($this->str, $i+4, strlen($this->str));
+                
+        //     //     // echo "\r\n\r\n<br><br><br>----";
+        //     //     // echo substr($this->str, $i, 4);
+        //     //     // echo "----<br><br><br>\r\n\r\n";
+        //     //     continue;
+        //     // }
             
-            // Leave trailing "\r\n\r\n" so it can be picked up for next paragraph, potentionally picks up a heading in it, best i got for now
-            if( $i !== false && $j !== false ) $this->str = substr($this->str, 0, $i) . "<p>" . substr($this->str, $i+4, $j - $i - 4) . "</p>\n" . substr($this->str, $j, strlen($this->str));
-            else break;
-        } while(1);
+        //     // Leave trailing "\r\n\r\n" so it can be picked up for next paragraph, potentionally picks up a heading in it, best i got for now
+        //     if( $i !== false && $j !== false ) $this->str = substr($this->str, 0, $i) . "<p>" . substr($this->str, $i+4, $j - $i - 4) . "</p>\n" . substr($this->str, $j, strlen($this->str));
+        //     else break;
+        // } while(1);
+        // $this->findAndSubstitute(0, 4, -4, 0, "\r\n\r\n", "\r\n\r\n", 4, "<p>", "</p>");
+    }
+
+    private function findAndSubstituteBlock(string $openMarkBlockPrefix) {
+        $depth = 0;
+        while( $this->substitueBlock($openMarkBlockPrefix) && 5 > $depth++){
+            //Nothing
+        }
+
+    }
+
+    private function substitueBlock(string $openMarkBlockPrefix ) {
+        $i = strpos($this->str, $openMarkBlockPrefix . "[");
+        $j = strpos($this->str, "]", $i + 1);
+
+        $text = "";
+
+        $offset = $openMarkBlockPrefix === "" ? 1 : 2;
+        if($i === false && $j === false) return false;
+        $text = substr($this->str, $i + $offset, $j - $i - $offset);
         
+        $urlOpen = strpos($this->str, "(", $j);
+        $urlClose = strpos($this->str, ")", $urlOpen + 1);
+
+        if( $urlOpen === false && $urlClose == false) return false;
+        $url = substr($this->str, $urlOpen + 1, $urlClose - $urlOpen - 1) ;
+
+        $insert = $openMarkBlockPrefix === "" ? 
+            "<a href=\"" . $url . "\">" . $text . "</a>" :
+            "<img class=\"blogPostImage\" src=\"" . $url . "\" alt=\"" . $text . "\">"; 
+        
+        // $urlClose + 1 to move of closing ) on url
+        $this->insertOne($i, $insert, $urlClose + 1);
+        return true;
+    }
+
+    private function insertOne(int $prefixStrEnd, string $insert, int $suffixStrStart) {
+        $this->str = 
+            substr($this->str, 0, $prefixStrEnd) . 
+            $insert .
+            substr($this->str, $suffixStrStart, strlen($this->str));
     }
 
     private function findAndSubstitute(int $start_endOffset, int $mid_stratOffest, int $mid_endOffset, int $end_startOffset, 
@@ -184,3 +190,4 @@ class MarkDownToHtml{
         }
     }
 }
+
